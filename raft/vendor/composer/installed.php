3200579987086964b57f554ee51cb0d95cf60056
Sean Murphy <sean@iamseanmurphy.com>
2025-09-05 12:20:06 +0000

    1.1.10

--- raft/vendor/composer/installed.php
+++ raft/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'codeinwp/raft',
-        'pretty_version' => 'v1.1.9',
-        'version' => '1.1.9.0',
-        'reference' => 'c2ce82c0a10c55272fb6c9cbe91c69ec5d865345',
+        'pretty_version' => 'v1.1.10',
+        'version' => '1.1.10.0',
+        'reference' => 'afcd5af61272a70d2fb59b4ec518662c3421976d',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,18 +11,18 @@
     ),
     'versions' => array(
         'codeinwp/raft' => array(
-            'pretty_version' => 'v1.1.9',
-            'version' => '1.1.9.0',
-            'reference' => 'c2ce82c0a10c55272fb6c9cbe91c69ec5d865345',
+            'pretty_version' => 'v1.1.10',
+            'version' => '1.1.10.0',
+            'reference' => 'afcd5af61272a70d2fb59b4ec518662c3421976d',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
             'dev_requirement' => false,
         ),
         'codeinwp/themeisle-sdk' => array(
-            'pretty_version' => '3.3.46',
-            'version' => '3.3.46.0',
-            'reference' => 'af2e7360bbe3af6a1bd2c3779fc33b7765104461',
+            'pretty_version' => '3.3.48',
+            'version' => '3.3.48.0',
+            'reference' => '0727d2cf2fc9bfb81b42968aeaf2bf4e340f021e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
             'aliases' => array(),
